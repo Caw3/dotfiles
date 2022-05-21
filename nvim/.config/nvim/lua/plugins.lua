caf5ab105ff0f15e2843a4f24b9ac1451dfed918
Caw3 <carl@willman.nu>
2022-05-21 14:36:24 +0200

    Tmux integration

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -44,10 +44,8 @@ return require("packer").startup(function(use)
 
 	--Cosmetic
 	use({ "nvim-lualine/lualine.nvim", config = [[require('config.lualine')]] })
-	-- use({ "seblj/nvim-tabline", config = [[require('config.tabline')]] })
 	use({ "tomasiser/vim-code-dark" })
 	use({ "norcalli/nvim-colorizer.lua", config = [[require('config.colorizer')]] })
-	-- use({ "lukas-reineke/indent-blankline.nvim", config = [[require("config.indent-blankline")]] })
 
 	--File navigation
 	use({
@@ -57,6 +55,7 @@ return require("packer").startup(function(use)
 		end,
 	})
 	use({ "junegunn/fzf.vim" })
+	use({ "christoomey/vim-tmux-navigator" })
 	use({ "elihunter173/dirbuf.nvim" })
 	use({
 		"nvim-telescope/telescope.nvim",
